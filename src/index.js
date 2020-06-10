52904a4c8d16ecfaca51387879408c4ff75a784c
Sofia Tofigh <sofiatofigh0@gmail.com>
2020-06-10 11:14:07 -0400

    not appending

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+
+
 document.addEventListener('DOMContentLoaded', () => {
+    fetchDogs();
 
 })
+
+const dogsUrl = `http://localhost:3000/dogs`
+const table = document.getElementById("table-body")
+console.log(table)
+
+
+
+
+function fetchDogs(){
+    fetch(dogsUrl)
+    .then(response => response.json())
+    .then(dogs => dogs.forEach(dog=> {
+        const tableRow = document.createElement('tr')
+        tableRow.innerHTML = `<td>${dog.name}</td>
+        <td>${dog.breed}</td>
+        <td>${dog.sex}</td>
+        <td><button type="click">Edit this dog!</button></td>`
+    // table.append(tableRow)
+}))}
